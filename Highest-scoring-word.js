59a810023ce3466985e80ae24432c24c96caa913
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-10-07 13:20:35 -0700

    Update Highest-scoring-word.js

--- Highest-scoring-word.js
+++ Highest-scoring-word.js
@@ -11,3 +11,8 @@ All letters will be lowercase and all inputs will be valid.
 
 
 // Solution
+
+function high(s){
+  let as = s.split(' ').map(s=>[...s].reduce((a,b)=>a+b.charCodeAt(0)-96,0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+}
